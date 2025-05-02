0288375b2054037c5fb5953a926defce023483d2
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-05-02 12:23:35 +0900

    프로젝트 설정 : feat : 프로젝트 기본 설정 https://github.com/Cassiiopeia/suh-grass-planter/issues/1

---
+++ src/main/java/me/suhsaechan/suhgrassplanter/SuhGrassPlanterApplication.java
@@ -0,0 +1,13 @@
+package me.suhsaechan.suhgrassplanter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhGrassPlanterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SuhGrassPlanterApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/suhsaechan/suhgrassplanter/SuhGrassPlanterApplicationTests.java
@@ -0,0 +1,13 @@
+package me.suhsaechan.suhgrassplanter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuhGrassPlanterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
