80266b21dbe0de54eb857961d63824f53e972f04
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-05-02 17:40:21 +0800

    프로젝트 설정 : fix : 존재하지않는 속성 쿼리분 삭제, 이젠 프로젝트 url 최신프로젝트로 업데이트 https://github.com/Cassiiopeia/suh-grass-planter/issues/1

--- src/main/java/me/suhsaechan/suhgrassplanter/repository/MemberRepository.java
+++ src/main/java/me/suhsaechan/suhgrassplanter/repository/MemberRepository.java
@@ -7,11 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, UUID> {
   Boolean existsByEmail(String email);
-
-  Boolean existsByUsername(String username);
   Boolean existsByNickname(String nickname);
-
-  Optional<Member> findByUsername(String username);
   Optional<Member> findByEmail(String email);
 
 }
